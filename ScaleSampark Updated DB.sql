a2b03d866c3d71ed6dd873a06a223288f2f81a34
Shrikant <shreekant.gadekar@gmail.com>
2020-01-29 02:53:50 +0530

    Added updated DB sql file and updated API document.

---
+++ ScaleSampark Updated DB.sql
@@ -0,0 +1,175 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
+--
+-- Host: 127.0.0.1    Database: scalesampark
+-- ------------------------------------------------------
+-- Server version	5.6.47-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(450) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (26,'Shrikant Gadekar','shreekant.gadekar@gmail.com'),(27,'Amol Jadhav','amol_jadhav@gmail.com'),(28,'Kiran Mhaske','kiran9076@gmail.com'),(29,'Sharda Kubhakaran','skumbha@gmail.com'),(30,'Varsha Ghule','ghulev1990@gmail.com'),(31,'Amruta Sutar','ansutar@gmail.com'),(32,'Mohit Chordiya','mohit.c@gmail.com'),(33,'Madan Pawar','madanpawar@gmail.com'),(34,'Shubham Vishvakarma','shubbham.vishva@gmail.com'),(35,'Shoeb Mohhammad','shoeb_m@gmail.com');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (87);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `message_type_id` bigint(20) DEFAULT NULL,
+  `message` varchar(1500) DEFAULT NULL,
+  `created_on` timestamp NULL DEFAULT NULL,
+  `message_uuid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `participant_uuid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`message_uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,'gn1F5KivEgfjHgK5TRdNZw==','2020-01-28 21:05:47',82,69),(1,'0KdMnax3nEHQELvXpwAP0Q==','2020-01-28 21:05:52',83,69),(1,'eax5gMMUHW+QL5GRcKnVoA==','2020-01-28 21:06:02',84,70),(1,'KaoFZOCe3BbdHW0j3gKetQ==','2020-01-28 21:06:19',85,70),(1,'PN/qF2WTQUMmlUaF7568Pg==','2020-01-28 21:06:28',86,69);
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message_type`
+--
+
+DROP TABLE IF EXISTS `message_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message_typecol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message_type`
+--
+
+LOCK TABLES `message_type` WRITE;
+/*!40000 ALTER TABLE `message_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messagetype`
+--
+
+DROP TABLE IF EXISTS `messagetype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messagetype` (
+  `id` bigint(20) NOT NULL,
+  `messageTypeName` varchar(255) DEFAULT NULL,
+  `message_type_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messagetype`
+--
+
+LOCK TABLES `messagetype` WRITE;
+/*!40000 ALTER TABLE `messagetype` DISABLE KEYS */;
+INSERT INTO `messagetype` VALUES (47,NULL,'openmessage');
+/*!40000 ALTER TABLE `messagetype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `participant`
+--
+
+DROP TABLE IF EXISTS `participant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `participant` (
+  `participant_uuid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `nickname` varchar(45) NOT NULL,
+  `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`participant_uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `participant`
+--
+
+LOCK TABLES `participant` WRITE;
+/*!40000 ALTER TABLE `participant` DISABLE KEYS */;
+INSERT INTO `participant` VALUES (69,'shreekant.gadekar@gmail.com','Shree','2020-01-28 21:08:04'),(70,'amol_jadhav@gmail.com','Amol','2020-01-28 21:07:52'),(71,'kiran9076@gmail.com','Amol','2020-01-28 21:07:33');
+/*!40000 ALTER TABLE `participant` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-29  2:43:42
